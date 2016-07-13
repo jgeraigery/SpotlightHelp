8418dbf1b868c49d879cbd0db25f9ae49964d90d
ejhinde <elizabeth.hinde@software.dell.com>
2016-07-13 16:32:23 +1000

    SOSSE-5660 Online Help Prototype

--- _balloonhelp/ConnectHyperv/component_version.md
+++
@@ -1,5 +0,0 @@
----
-title: Version component
-summary: "Show the version of the Hyper-V server."
-sidebar: c_hyperv_sidebar
----
---
+++ balloonhelp.bat
@@ -0,0 +1,3 @@
+echo "Building Balloonhelp";
+jekyll serve --config _configBalloonhelp.yml --destination _siteBalloonhelp --no-watch;
+echo "done";
