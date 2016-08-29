27c53803c32c553895c8f73b6102549c25a1a4b9
Steven Taylor <steven.taylor@quest.com>
2016-08-29 15:08:56 +1000

    SOSSE-5851 - generate wix script for help files

--- Build/BuildHelp.groovy
+++ Build/BuildHelp.groovy
@@ -7,6 +7,10 @@ def BuildHelp()
 
         bat """jekyll build --config "${HelpDir}\\_configOffline.yml" -s "${HelpDir}" -d "${HelpDir}\\spotlightHelp_site" """
         bat """jekyll build --config "${HelpDir}\\_configBalloonHelpOffline.yml" -s "${HelpDir}" -d "${HelpDir}\\spotlightHelp_siteBalloonHelp" """
+        
+        bat """heat dir "${HelpDir}\\spotlightHelp_site" -o "${HelpDir}\\helpsite.wxs" -scom -sfrag -srd -sreg -gg -cg HelpSiteGroup -dr HELP_SITE_DIR """
+        bat """heat dir "${HelpDir}\\spotlightHelp_siteBalloonHelp" -o "${HelpDir}\\balloonhelpsite.wxs" -scom -sfrag -srd -sreg -gg -cg BalloonHelpSiteGroup -dr BALLOONHELP_SITE_DIR """
+        
     }
     catch (any)
     {
