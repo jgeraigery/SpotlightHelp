f6ca8ed646cc3903697f5fd3eb3efd0a2d6afb9c
Sebastian Godelet <Sebastian.Godelet@quest.com>
2017-02-14 14:47:24 +0800

    [SOSSE-6306] Allow building help from command line

--- _build/BuildHelp.groovy
+++ _build/BuildHelp.groovy
@@ -4,18 +4,7 @@ def BuildHelp()
     {
         stage 'Build Help'
         def HelpDir=pwd()
-        def wixToolsetBin="C:\\Program Files (x86)\\WiX Toolset v3.9\\bin" 
-
-        // jekyll builds - folders starting with '_' will not be included in Jekyll outputs
-        bat """jekyll build --config "${HelpDir}\\_configOffline.yml" -s "${HelpDir}" -d "${HelpDir}\\_site" """
-        bat """jekyll build --config "${HelpDir}\\_configBalloonHelpOffline.yml" -s "${HelpDir}" -d "${HelpDir}\\_siteBalloonHelp" """
-
-
-        // Wix fragements for installing help
-        bat """"${wixToolsetBin}\\heat.exe" dir "${HelpDir}\\_site" -o "${HelpDir}\\_install\\helpsite.wxs" -scom -sfrag -srd -sreg -gg -cg HelpSiteGroup -dr HELP_SITE_DIR """
-        bat """"${wixToolsetBin}\\heat.exe" dir "${HelpDir}\\_siteBalloonHelp" -o "${HelpDir}\\_install\\balloonhelpsite.wxs" -scom -sfrag -srd -sreg -gg -cg BalloonHelpSiteGroup -dr BALLOONHELP_SITE_DIR """
-        // map file for client F1
-        bat """"${wixToolsetBin}\\heat.exe" file "${HelpDir}\\_install\\csh.map" -o "${HelpDir}\\_install\\helpmap.wxs" -scom -sfrag -srd -sreg -gg -cg HelpMapGroup -dr Help_dir """
+        bat """call "${HelpDir}\\_build\\BuildHelp.cmd""""
         
     }
     catch (any)
