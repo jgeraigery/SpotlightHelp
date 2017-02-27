b2059fc322c53133d312a884bd578c0a509254fa
charles.wang <charles.wang@software.dell.com>
2017-02-27 23:40:57 +0800

    update script

--- _build/scripts/spotlight-help-deploy-to-balloon.ps1
+++ _build/scripts/spotlight-help-deploy-to-balloon.ps1
@@ -5,4 +5,4 @@ Param($project_root = "",
 $secpasswd = ConvertTo-SecureString "$($ENV:AzurePassword)" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("3a99e1f6-7e90-426e-b399-3536f078f9ca", $secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 366f6586-9b69-47bd-bfb3-59845478627f -Credential $mycreds
-& "${project.root}\_build\tools\AzCopy.exe" "${project.root}\_site" ${blobContainer} ${destkey} /S
+& "${project.root}\_build\tools\AzCopy.exe" "${project_root}\_site" ${blobContainer} ${destkey} /S
