928f950156453c24bf957fe0f1cf5a64633cbf8f
charles.wang <charles.wang@software.dell.com>
2017-03-07 10:13:17 +0800

    update scripts

--- _build/scripts/spotlight-help-deploy-to-balloon-ftp.ps1
+++ _build/scripts/spotlight-help-deploy-to-balloon-ftp.ps1
@@ -5,66 +5,50 @@ Param($project_root = "",
 
 #Directory where to find pictures to upload
 $UploadFolder = "$project_root\_site\"
-
-function UploadFilesOfFolder($targetfolder, $user, $passwd, $ftphost, $localBase)
+function uploadFiles($folder, $destFolder)
 {
-  $webclient = New-Object System.Net.WebClient
-  $webclient.Credentials = New-Object System.Net.NetworkCredential($user,$passwd)
-  Write-Output $targetfolder
-  $SrcEntries = Get-ChildItem $targetfolder
-  $Srcfolders = $SrcEntries | Where-Object{$_.PSIsContainer}
-  $SrcFiles = $SrcEntries | Where-Object{!$_.PSIsContainer}
-
-  # Upload Files - Start
-  foreach($entry in $SrcEntries)
+  $folder = $folder.Trim()
+  $files = Get-ChildItem $folder
+  foreach($file in $files)
   {
-    Write-Output "target:$entry"
-    if ($entry -is [System.IO.FileInfo]) {
-      $SrcFullname = $entry.fullname
-      $SrcName = $entry.Name
-      $SrcFilePath = $targetFolder -replace "\\","\\" -replace "\:","\:"
-      $DesFile = $SrcFullname -replace $SrcFilePath,$ftphost
-      $DesFile = $DesFile -replace "\\", "/"
-      Write-Output $DesFile
-      Write-Output $SrcFullname
-
-      $uri = New-Object System.Uri($DesFile)
-      $webclient.UploadFile($uri, $SrcFullname)
-    } else {
-      $srcPath = "$targetfolder$entry\"
-      $SrcFolderPath = $srcPath  -replace "\\","\\" -replace "\:","\:"
-
-      $DesFolder = $SrcFolderPath -replace $localBase, $ftphost
-      $DesFolder = $DesFolder -replace "\\", "/"
-      Write-Output $DesFolder
-
-      try
+    write-host "$file"
+    if($file -is [System.IO.FileInfo])
+    {
+      Write-Output "$file is file"
+      $fileName = "$folder\$file"
+      if ($destFolder -eq "")
       {
-        $makeDirectory = [System.Net.WebRequest]::Create($DesFolder);
+        $blobName = "$file"
+      }
+      else
+      {
+        $blobName = "$destFolder/$file"
+      }
+      Write-Output "copying $fileName to $blobName"
+      $webclient = New-Object System.Net.WebClient
+      $webclient.Credentials = New-Object System.Net.NetworkCredential($user,$passwd)
+      $uri = New-Object System.Uri($DesFile)
+      $webclient.UploadFile("$FTPHost$blobName", $fileName)
+    }
+    else
+    {
+      Write-Output "$file is folder"
+      if ($destFolder -eq "")
+      {
+        uploadFiles -folder "$folder\$file" -destFolder $file
+      }
+      else
+      {
+        Write-Output "create folder:$FTPHost$destFolder"
+        $makeDirectory = [System.Net.WebRequest]::Create("$FTPHost$destFolder");
         $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($user,$passwd);
         $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
         $makeDirectory.GetResponse();
         #folder created successfully
         Write-Output 'Done'
-
-        UploadFilesOfFolder -targetfolder $srcPath -user $user -passwd $passwd -ftphost $ftphost, -localBase $localBase
-      }
-      catch [Net.WebException]
-      {
-          try {
-            #if there was an error returned, check if folder already existed on server
-            $checkDirectory = [System.Net.WebRequest]::Create($DesFolder);
-            $checkDirectory.Credentials = New-Object System.Net.NetworkCredential($user,$passwd);
-            $checkDirectory.Method = [System.Net.WebRequestMethods+FTP]::PrintWorkingDirectory;
-            $response = $checkDirectory.GetResponse();
-            #folder already exists!
-          }
-          catch [Net.WebException] {
-              #if the folder didn't exist
-          }
+        uploadFiles -folder "$folder\$file" -destFolder "$destFolder/$file"
       }
     }
   }
 }
-$basePath = $UploadFolder -replace "\\","\\" -replace "\:","\:"
-UploadFilesOfFolder -targetfolder $UploadFolder -user $FTPUser -passwd $FTPPass -ftphost $FTPHost, -localBase $basePath
+uploadFiles -folder $localfolder -destFolder ""
